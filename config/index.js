bf01b456653e5b8713cbbc77635d984c1a98da35
bkleeman <benjamin.kleeman@gmail.com>
2019-11-14 17:17:46 -0600

    unify secret reference

--- config/index.js
+++ config/index.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+// };
+
+
 module.exports = {
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'conduit'
 };
